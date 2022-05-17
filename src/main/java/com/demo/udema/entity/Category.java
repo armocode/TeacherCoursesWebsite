84ac012dad7ea12699eb5992065c12a2298b8c47
InFM8 <andriusm4@gmail.com>
2022-05-17 13:37:05 +0300

    upd

--- src/main/java/com/demo/udema/entity/Category.java
+++ src/main/java/com/demo/udema/entity/Category.java
@@ -16,4 +16,41 @@ public class Category {
 
     @OneToOne(mappedBy = "category", cascade = CascadeType.ALL)
     private Course course;
+
+    public Category() {}
+
+    public Category(String title, Course course) {
+        this.title = title;
+        this.course = course;
+    }
+
+    public Category(int id, String title, Course course) {
+        this.id = id;
+        this.title = title;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
--- src/main/java/com/demo/udema/entity/Course.java
+++ src/main/java/com/demo/udema/entity/Course.java
@@ -27,4 +27,84 @@ public class Course {
 
     @Column(name = "kaina")
     private double price;
+
+    public Course() {}
+
+    public Course(int id, String data, User users, String title, Category category, double price) {
+        this.id = id;
+        this.data = data;
+        this.users = users;
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Course(String data, User users, String title, Category category, double price) {
+        this.data = data;
+        this.users = users;
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Course(User users, String title, Category category, double price) {
+        this.users = users;
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Course(String title, Category category, double price) {
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public User getUsers() {
+        return users;
+    }
+
+    public void setUsers(User users) {
+        this.users = users;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
