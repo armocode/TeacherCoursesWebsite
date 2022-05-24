2a8746e4b5b35896ad3bc584475f6e5bb1c9e972
InFM8 <andriusm4@gmail.com>
2022-05-24 18:47:14 +0300

    backup v2

--- src/main/java/com/demo/udema/controller/CourseController.java
+++ src/main/java/com/demo/udema/controller/CourseController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class CourseController {
@@ -28,5 +30,18 @@ public class CourseController {
     }
 
 
+//    @GetMapping("/searchForCourse")
+//   public String searchForCourse(@ModelAttribute("courseForm") Course course, Model model, RedirectAttributes redirectAttributes) {
+//        model.addAttribute("courseForm", course);
+//        if(course.getTitle().toLowerCase(Locale.ROOT).equals("java")) {
+//            redirectAttributes.addFlashAttribute("error", "case 1");
+//            return "redirect:/list";
+//        } else if (course.getTitle().toLowerCase(Locale.ROOT).equals("")) {
+//            redirectAttributes.addFlashAttribute("error", "case 2");
+//            return "redirect:/index";
+//        }
+//       redirectAttributes.addFlashAttribute("error", "case 3");
+//       return "redirect:/index";
+//   }
 
 }
