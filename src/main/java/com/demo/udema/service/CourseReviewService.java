710d9f36e422c5488483f9bee545690697091e7e
InFM8 <andriusm4@gmail.com>
2022-05-27 13:18:14 +0300

    backup Commit

--- src/main/java/com/demo/udema/service/CourseReviewService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.udema.service;
-
-import com.demo.udema.entity.CourseReviews;
-
-import java.util.List;
-
-public interface CourseReviewService {
-    List<CourseReviews> findAll();
-}
--- src/main/java/com/demo/udema/service/SecurityService.java
+++
@@ -1,6 +0,0 @@
-package com.demo.udema.service;
-
-public interface SecurityService {
-    boolean isAuthenticated();
-    void autoLogin(String username, String password);
-}
